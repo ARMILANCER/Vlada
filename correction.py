f25ead93b0ca5e9928dadb6417feb5de73583a88
ARMILANCER <114140097+ARMILANCER@users.noreply.github.com>
2023-10-22 12:21:22 +0200

    Add files via upload

---
+++ correction.py
@@ -0,0 +1,30 @@
+print ("× =")
+x = eval (input ("inserisci il valore "))
+print ("y =")
+y = eval (input ("inserisci il valore "))
+
+
+# 'z' has not been initialized
+# prints only the message not the values
+
+print ("z =")
+
+# input is ised to inser a value of keyboard,
+# we already have X and Y, you don't need any other values.
+#z = eval (input (x+y) )
+
+z = x +y
+print (z)
+
+#z = eval(input (x-y) )
+
+z = x - y
+print (z)
+#z = eval (input (x*y) )
+
+z = x * y
+print (z)
+#z = eval (input (x/y))
+
+z = x /y
+print (z)
---
+++ operators.py
@@ -0,0 +1,27 @@
+# By Christian Brito.
+
+### appunti
+# val -> string interpreter
+###
+
+# create the variable (value n),
+# define the type of input (int())  
+value1 = int(input("Inser a number: "))
+value2 = int(input("Inser a number: "))
+# in a program the instruttion are sequenzial,
+# has this strutture -> input(data) -> elaboration(/,*,+,-)
+# -> Assignment(operation = elaboration) -> output print(f"Division: {operation}")
+operation = value1 - value2;
+print(f"Subtraction: {operation}")
+operation = value1 + value2;
+print(f"Sum: {operation}")
+operation = operation = value1 * value2;
+print(f"Multiplication: {operation}")
+operation = operation = value1 / value2;
+print(f"Division: {operation}")
+
+# Nota:
+## posso consigliarti l'utilizzo di chatbot come chatGPT,
+# è molto utile per la programmazione, perchè ti fa vedere direttamente il codice,
+# basta che tu inserisca una istruzione, nel caso son sempre contento di darti una mano(ne ho due ^_^)
+## 
